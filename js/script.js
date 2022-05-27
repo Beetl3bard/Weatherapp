9a43597957d4deac6470f9d7d8acac0b06582794
Josh West <trademasterwong@gmail.com>
2022-05-27 16:06:16 -0500

    appending data

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-var search = document.querySelector('search');
+var search = document.querySelector('.search');
+var cityInput=document.querySelector("#searchbox")
 
-search.addEventListener("click", function() {
-    console.log('clicked')
+search.addEventListener("click", function(event) {
+    event.preventDefault();
+    var city = cityInput.value
+    //console.log(city)
+    weatherApi(city);
   });
 
 
-var weatherApi = function() {
+var weatherApi = function(city) {
     
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=memphis&appid=f315da550213c8aba9a006208bd196d6&units=imperial";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=f315da550213c8aba9a006208bd196d6&units=imperial";
   
     // make a get request to url
     fetch(apiUrl)
@@ -16,15 +20,27 @@ var weatherApi = function() {
         if (response.ok) {
           console.log(response);
           response.json().then(function(data) {
+            currentdate(data);
             console.log(data);
-            
+           
           });
         } else {
           alert('Error: API Endpoint Not Found');
         }
       })
       .catch(function(error) {
-        alert("Unable to connect to API");
+        alert("Unable to connect");
       });
   };
-weatherApi();
+
+  function currentdate(data){
+    var date= new Date(data.dt*1000).toLocaleDateString()
+    var wind= data.wind.speed
+    console.log(date);
+    console.log(wind);
+
+    var currentcity = document.querySelector(".currentcity");
+    currentcity.textContent = "{"+date+"}"
+    //windspeed.textContent = "{"+wind+ "}"
+  }
+  
